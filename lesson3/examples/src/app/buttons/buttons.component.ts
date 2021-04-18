422776b4b33c1e0fcbff2ac73b77729226f3208e
Justin Portell <72322829+jupo-ex-it@users.noreply.github.com>
2021-04-17 20:51:33 -0500

    different, but similar outcome

--- lesson3/examples/src/app/buttons/buttons.component.ts
+++ lesson3/examples/src/app/buttons/buttons.component.ts
@@ -7,16 +7,17 @@ import { Component, OnInit } from "@angular/core";
 })
 export class ButtonsComponent implements OnInit {
   buttonHeading: string = "Buttons";
-  inactive: boolean = false;
+  goldInactive: boolean = false;
+  silverInactive: boolean = false;
+  copperInactive: boolean = false;
 
   constructor() {}
 
   ngOnInit() {}
 
-
-
-  deactivate(button): void {
-    button.style = false;
-    button.disabled = true;
+  reactivate(): void {
+    this.goldInactive = false;
+    this.silverInactive = false;
+    this.copperInactive = false;
   }
 }
